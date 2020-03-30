a96babe1f3d51b5e10858ccc2f51e1f835d5f9ee
vinay <167894+simplyvinay@users.noreply.github.com>
2020-03-30 06:11:46 +0100

    added NewtonsoftJson and started adding infra code

---
+++ Infrastructure/Entity.cs
@@ -0,0 +1,44 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace ApiStarter
+{
+    public interface IEntity
+    {
+        int Id { get; }
+    }
+
+    public interface IArchivableEntity
+    {
+        bool Archived { get; }
+        void Archive();
+        void UnArchive();
+    }
+
+    public class Entity : IEntity
+    {
+        [JsonIgnore]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class ArchivableEntity : Entity, IArchivableEntity
+    {
+        public bool Archived { get; set; }
+        
+        public void Archive()
+        {
+            Archived = true;
+        }
+
+        public void UnArchive()
+        {
+            Archived = false;
+        }
+    }
+}
